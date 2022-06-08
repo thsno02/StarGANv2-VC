a884240c0b1efd7f6d9df52cb8b1900beb24a260
thsno02 <taihangshan2022@gmail.com>
2022-06-08 14:25:33 +0800

    remove .bat

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 conda create -n vc python=3.9
-conda.bat activate vc
+conda activate vc
+# conda list
+# python --version
 pip install -r requirements.txt -i  https://pypi.doubanio.com/simple/  --trusted-host pypi.doubanio.com
+# conda list
 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
