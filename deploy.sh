15b4b3e993aa2f2424c9030c01f0afdb50e79ad8
thsno02 <taihangshan2022@gmail.com>
2022-06-08 13:13:31 +0800

    modify conda activate comman

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 conda create -n vc python=3.9
-conda activate vc
+conda.bat activate vc
 pip install -r requirements.txt -i  https://pypi.doubanio.com/simple/  --trusted-host pypi.doubanio.com
 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
