9006e71f68219bc3c0248b22f2af826025632c29
thsno02 <taihangshan2022@gmail.com>
2022-07-21 15:48:17 +0800

    remove useless code

--- device_test.py
+++ device_test.py
@@ -1,17 +1,51 @@
 import sounddevice as sd
+import numpy as np
+import time
+import queue
 
-# set input and output devices
-sd.default.device = 0, 1
+np.set_printoptions(threshold=np.inf)
+
+sd._terminate()
+sd._initialize()
+sd.default.reset()  # reset all the default setting
 fs = 24000
-sd.default.samplerate = fs # set sample rate
-sd.default.channels = 1, 2 # one input channel, two output channel
+sd.default.samplerate = fs  # specify sample rate
+sd.default.dtype = 'float32', 'float32'  # specify data type
+sd.default.channels = 1, 2  # specify the input/output channels
 
-duration = 10 # seconds
+def callback(in_data, frames, time, status):
+    """This is called (from a separate thread) for each audio block."""
+    q.put(in_data.copy())
 
-print('begin recording')
-# record voice
-audio = sd.rec(int(duration * fs), dtype = 'float32')
-sd.wait() # wait to recording
-print('begin playing')
-sd.play(audio, fs)
-sd.wait()
+
+while True:
+    audio = np.zeros(shape=(24000, 1), dtype='float32')
+    q = queue.Queue()
+    # set input and output device
+    print(sd.query_devices())
+    input_device = int(input('plz type the input device:\t'))
+    output_device = int(input('plz type the output device:\t'))
+    sd.default.device = input_device, output_device
+
+    try:
+        with sd.InputStream(samplerate=24000,
+                            dtype='float32',
+                            channels=1,
+                            callback=callback):
+            start_time = time.time()
+
+            print('#' * 80)
+            print('press Ctrl+C to stop the recording')
+            print('#' * 80)
+            while True:
+                audio = np.append(audio, q.get(), axis=0)
+    except KeyboardInterrupt:
+        end_time = time.time()
+        print('\nRecording finished: costs {} {}'.format(end_time - start_time,
+                                                        np.shape(audio)))
+        start_time = time.time()
+        print(audio)
+        sd.play(audio)
+        sd.wait()
+        end_time = time.time()
+        print('\nPlaying finished: costs {}'.format(end_time - start_time))
